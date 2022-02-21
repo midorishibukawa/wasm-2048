8fab8bfb396cde767619a6c50e5cb2d4691974b8
midorishibukawa <contact@midori.dev>
2022-02-21 17:41:05 -0300

    add cell colors and minor clean up

--- www/index.js
+++ www/index.js
@@ -5,18 +5,27 @@ const boardSize = 4;
 const gameBoard = new GameBoard(boardSize);
 const canvas = document.querySelector('#c2048');
 const margin = 128;
-const canvasSize = Math.min(window.innerHeight, window.innerWidth) - margin;
-const ctx = canvas.getContext('2d');
-const fontName = 'monospace';
-const gap = canvasSize / 64;
-const fontSize = (canvasSize / boardSize - gap) / 4;
-const squareSize = (canvasSize - gap * (boardSize + 1)) / boardSize;
+const fontName = 'Open Sans';
+const canvas = document.querySelector('#c2048');
 
-canvas.height = canvasSize;
-canvas.width = canvasSize;
-ctx.font = `${fontSize}px ${fontName}`;
+let boardSize, gameBoard, canvasSize, ctx, gap, fontSize, squareSize, borderRadius;
 
-const init = () => {
+const init = (size) => {
+    boardSize = 4;
+    gameBoard = new GameBoard(boardSize);
+    canvasSize = Math.min(window.innerHeight, window.innerWidth) - margin;
+    ctx = canvas.getContext('2d');
+    gap = canvasSize / 64;
+    fontSize = (canvasSize / boardSize - gap) / 2.5;
+    squareSize = (canvasSize - gap * (boardSize + 1)) / boardSize;
+    borderRadius = canvasSize / 200;
+
+    console.log(`boardSize = ${boardSize}\ncanvasSize = ${canvasSize}\ngap = ${gap}\nfontSize = ${fontSize}\nsquareSize = ${squareSize}\nborderRadius = ${borderRadius}`);
+    
+    canvas.height = canvasSize;
+    canvas.width = canvasSize;
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
     gameBoard.generate();
     render();
 }
@@ -36,29 +45,54 @@ const move = (key) => {
 
 const render = () => {
     const cells = new Uint32Array(memory.buffer, gameBoard.cells, boardSize * boardSize);
-    
+    renderBoard();
+    renderCells(cells);
+}
+
+const renderBoard = () => {
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
-            ctx.strokeStyle = '#eee';
-            ctx.fillStyle = '#eee';
+            ctx.strokeStyle = '#cdc1b4';
+            ctx.fillStyle = '#cdc1b4';
             let x = j * squareSize + (j + 1) * gap;
             let y = i * squareSize + (i + 1) * gap;
-            roundRect(ctx, x, y, squareSize, 4);
+            roundRect(ctx, x, y, squareSize, borderRadius);
         }
     }
+}
 
+const renderCells = (cells) => {
     for (let i = 0; i < boardSize; i++) {
         for (let j = 0; j < boardSize; j++) {
             let value = cells[i * boardSize + j];
             if (value == 0) continue;
+            let cellColor = color(value);
             let x = j * squareSize + (j + 1) * gap;
             let y = i * squareSize + (i + 1) * gap;
-            ctx.strokeStyle = '#ddd';
-            ctx.fillStyle = '#ddd';
-            roundRect(ctx, x, y, squareSize, 4);
-            ctx.fillStyle = '#000';
-            ctx.fillText(value, x + squareSize / 2 - fontSize / 3, y + squareSize / 2 + fontSize / 3);
+            ctx.strokeStyle = cellColor;
+            ctx.fillStyle = cellColor;
+            roundRect(ctx, x, y, squareSize, borderRadius);
+            ctx.fillStyle = value <= 4 ? '#776e65' : '#f9f6f2';
+            const fontS = value < 100 ? fontSize : value < 1000 ? fontSize / 1.1 : fontSize / 1.2;
+            ctx.font = `${fontS}px ${fontName}`;
+            ctx.fillText(value, x + squareSize / 2, y + squareSize / 2);
         }
+    }
+}
+
+const color = (value) => {
+    switch (value) {
+        case 2: return '#eee4da';
+        case 4: return '#eee1c9';
+        case 8: return '#f3b27a';
+        case 16: return '#f69664';
+        case 32: return '#f77c5f';
+        case 64: return '#f75f3b';
+        case 128: return '#edd073';
+        case 256: return '#edcc62';
+        case 512: return '#edc950';
+        case 1024: return '#edc53f';
+        default: return '#eec22e';
     }
 }
 
@@ -70,10 +104,13 @@ const roundRect = (ctx, x, y, size, radius) => {
     
     ctx.lineTo(x + size - radius, y);
     ctx.quadraticCurveTo(x + size, y, x + size, y + radius);
+    
     ctx.lineTo(x + size, y + size - radius);
     ctx.quadraticCurveTo(x + size, y + size, x + size - radius, y + size);
+
     ctx.lineTo(x + radius, y + size);
     ctx.quadraticCurveTo(x, y + size, x, y + size - radius);
+    
     ctx.lineTo(x, y + radius);
     ctx.quadraticCurveTo(x, y, x + radius, y);
     
